3d1955fd60bdab86fea926b0fa228539cd1584af
Sk Sajidul Kadir <sheikh.sajid522@gmail.com>
2020-10-01 14:48:50 +1000

    Add editors and connect jsx2svelte compiler

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import AceEditor from 'react-ace';
+import { compile } from 'jsx2svelte';
 import './App.css';
 
+import 'ace-builds/src-noconflict/mode-jsx';
+import 'ace-builds/src-noconflict/mode-html';
+import 'ace-builds/src-noconflict/theme-github';
+
+const placeholderJSX = `import React, { useState } from 'react';
+
+// Note: jsx2svelte only supports hooks
+export default function Example() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
+    </div>
+  );
+}
+`;
+
 function App() {
+  const [jsxCode, setJsxCode] = useState(placeholderJSX);
+  const [svelteCode, setSvelteCode] = useState('');
+
+  const compileHandler = () => {
+    try {
+      setSvelteCode(compile(jsxCode));
+    } catch (e) {
+      alert(e);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AceEditor
+        className="jsx-code"
+        mode="jsx"
+        theme="github"
+        onChange={setJsxCode}
+        value={jsxCode}
+        name="UNIQUE_ID_OF_DIV"
+        editorProps={{ $blockScrolling: true }}
+      />
+
+      <button className="compile-button" onClick={compileHandler}>
+        Compile
+      </button>
+
+      <AceEditor
+        className="svelte-code"
+        mode="html"
+        theme="github"
+        value={svelteCode}
+        name="UNIQUE_ID_OF_DIV"
+        editorProps={{ $blockScrolling: true }}
+      />
     </div>
   );
 }
